5e515a667f6e089a22711e4af844f881e1341992
Felipe Pereira <felipe.pereira@logrock.com>
2022-02-23 11:41:41 -0300

    feat(project): :tada: restart project with preact
    BREAKING CHANGE: previous version made with react no longer used

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+const path = require('path')
+import preact from '@preact/preset-vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: '@logrock/pebbles',
+      fileName: (format) => `logrock-pebbles.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['preact'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          preact: 'Preact'
+        }
+      }
+    }
+  }
+})
