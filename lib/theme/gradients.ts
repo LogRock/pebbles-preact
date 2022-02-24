2006974f1d22499eafd8bdba418de23eef997f99
Felipe Pereira <felipe.pereira@logrock.com>
2022-02-24 10:44:56 -0300

    feat(theme): :lipstick: add theme with initial design tokens

---
+++ lib/theme/gradients.ts
@@ -0,0 +1,52 @@
+export const sunburst= {
+  colors: ['#FF5F6D', '#FFC371'],
+  angle: '98.41deg',
+}
+  export const darkWineSea= {
+  colors: ['#16BFFD', '#CB3066'],
+  angle: '98.41deg',
+}
+  export const cherryCola= {
+  colors: ['#EECDA3', '#EF629F'],
+  angle: '98.41deg',
+}
+  export const coldSky= {
+  colors: ['#4CA1AF', '#C4E0E5'],
+  angle: '98.41deg',
+}
+  export const oliveGarden= {
+  colors: ['#F1F2B5', '#135058', '#135058'],
+  angle: '98.41deg',
+}
+  export const morningSakura= {
+  colors: ['#FF6E7F', '#BFE9FF'],
+  angle: '98.41deg',
+}
+  export const blueMoon= {
+  colors: ['#1D2B64', '#F8CDDA'],
+  angle: '98.41deg',
+}
+  export const synthwave= {
+  colors: ['#FC00FF', '#00B6DE'],
+  angle: '98.41deg',
+}
+  export const teaLeaves= {
+  colors: ['#02AAB0', '#00CDAC'],
+  angle: '98.41deg',
+}
+  export const blueberryWine= {
+  colors: ['#4568DC', '#B06AB3'],
+  angle: '98.41deg',
+  }
+
+// MeshGradients still not CSS definable
+  // export const chillNight: MeshGradient
+  // export const sunrise: MeshGradient
+  // export const kingCrimsom: MeshGradient
+  // export const crazyDiamong: MeshGradient
+  // export const sunsetOverdrive: MeshGradient
+  // export const auroraBorealis: MeshGradient
+  // export const emeraldSplash: MeshGradient
+  // export const overcast: MeshGradient
+  // export const feverish: MeshGradient
+  // export const spriteCrush: MeshGradient
---
+++ lib/types/theme.ts
@@ -0,0 +1,157 @@
+import * as CSS from "csstype"
+
+interface Spacings {
+  xxsm: CSS.Property.Width,
+  xsm: CSS.Property.Width
+  sm: CSS.Property.Width
+  md: CSS.Property.Width
+  big: CSS.Property.Width
+  xbig: CSS.Property.Width
+  xxbig: CSS.Property.Width
+  xxxbig: CSS.Property.Width
+  lg: CSS.Property.Width
+  xlg: CSS.Property.Width
+  xxlg: CSS.Property.Width
+  xxxlg: CSS.Property.Width
+  huge: CSS.Property.Width
+  xhuge: CSS.Property.Width
+  xxhuge: CSS.Property.Width
+  xxxhuge: CSS.Property.Width
+}
+
+interface Color {
+  50: CSS.Property.Color
+  100: CSS.Property.Color
+  200: CSS.Property.Color
+  300: CSS.Property.Color
+  400: CSS.Property.Color
+  500: CSS.Property.Color
+  600: CSS.Property.Color
+  700: CSS.Property.Color
+  800: CSS.Property.Color
+  900: CSS.Property.Color
+}
+
+interface Colors {
+  neutral: Color
+  primary: Color
+  secondary: Color
+  success: Color
+  warning: Color
+  error: Color
+  shades: {
+    0: CSS.Property.Color
+    100: CSS.Property.Color
+  }
+}
+
+interface HeaderProps {
+  desktop: {
+    weights: {
+      normal: CSS.Property.FontWeight,
+      bold: CSS.Property.FontWeight,
+      bolder: CSS.Property.FontWeight,
+      boldest: CSS.Property.FontWeight
+    },
+    fontSize: CSS.Property.FontSize
+    lineHeight: CSS.Property.LineHeight 
+    tracking: CSS.Property.LetterSpacing
+  }
+  mobile: {
+    weights: {
+      normal: CSS.Property.FontWeight,
+      bold: CSS.Property.FontWeight,
+      bolder: CSS.Property.FontWeight,
+      boldest: CSS.Property.FontWeight
+    },
+    fontSize: CSS.Property.FontSize
+    lineHeight: CSS.Property.LineHeight 
+    tracking: CSS.Property.LetterSpacing
+  }
+}
+
+interface ParagraphProps {
+  fontSize: CSS.Property.FontSize
+  lineHeight: CSS.Property.LineHeight
+  weights: {
+    normal: CSS.Property.FontWeight,
+    bold: CSS.Property.FontWeight,
+    bolder: CSS.Property.FontWeight
+  }
+}
+
+interface Typography {
+  displayLarge: HeaderProps
+  displaySmall: HeaderProps
+  headingH1: HeaderProps
+  headingH2: HeaderProps
+  headingH3: HeaderProps
+  headingH4: HeaderProps
+  headingH5: HeaderProps
+  headingH6: HeaderProps
+  paragraphLarge: ParagraphProps
+  paragraphMedium: ParagraphProps
+  paragraphSmall: ParagraphProps
+  paragraphXSmall: ParagraphProps
+  overlineLarge: ParagraphProps
+  overlineSmall: ParagraphProps
+}
+
+interface Shadows {
+  xSmall: CSS.Property.BoxShadow
+  small: CSS.Property.BoxShadow
+  medium: CSS.Property.BoxShadow
+  large: CSS.Property.BoxShadow
+  xLarge: CSS.Property.BoxShadow
+  xxLarge: CSS.Property.BoxShadow
+}
+
+interface Blurs {
+  none: CSS.Property.Width
+  small: CSS.Property.Width
+  medium: CSS.Property.Width
+  large: CSS.Property.Width
+  xLarge: CSS.Property.Width
+}
+
+interface LinearGradient {
+  colors: CSS.Property.BackgroundColor[]
+  angle: CSS.Property.Rotate
+}
+
+interface MeshGradient {
+  background: CSS.Property.BackgroundColor
+  mesh: CSS.Property.BackgroundImage
+}
+
+interface Gradients {
+  sunburst: LinearGradient
+  darkWineSea: LinearGradient
+  cherryCola: LinearGradient
+  coldSky: LinearGradient
+  oliveGarden: LinearGradient
+  morningSakura: LinearGradient
+  blueMoon: LinearGradient
+  synthwave: LinearGradient
+  teaLeaves: LinearGradient
+  blueberryWine: LinearGradient
+  // chillNight: MeshGradient
+  // sunrise: MeshGradient
+  // kingCrimsom: MeshGradient
+  // crazyDiamond: MeshGradient
+  // sunsetOverdrive: MeshGradient
+  // auroraBorealis: MeshGradient
+  // emeraldSplash: MeshGradient
+  // overcast: MeshGradient
+  // feverish: MeshGradient
+  // spriteCrush: MeshGradient
+}
+
+export default interface Theme {
+  spacings: Spacings
+  colors: Colors
+  typography: Typography
+  shadows: Shadows
+  blurs: Blurs
+  gradients: Gradients
+}
