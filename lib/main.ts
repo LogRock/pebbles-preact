f2dfe42fc84127569b17fa810510ec958f94127a
Felipe Pereira <felipe.pereira@logrock.com>
2022-02-28 11:19:26 -0300

    chore(project): :rotating_light: add eslint setup

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,3 @@
 export type { default as Theme } from "./types/theme"
-export { light as lightTheme } from "./theme" 
+export { light as lightTheme } from "./theme"
 export { dark as darkTheme } from "./theme"
